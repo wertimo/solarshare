593b3b6a3143ceca87f9f9452e5fb612e179fef7
wertimo <wertekq@gmail.com>
2024-06-03 00:39:28 +0100

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-document.getElementById('joinWaitlistButton').onclick = function() {
-    document.getElementById('formModal').style.display = 'flex';
-}
-
-document.getElementsByClassName('close')[0].onclick = function() {
-    document.getElementById('formModal').style.display = 'none';
-}
-
-window.onclick = function(event) {
-    if (event.target == document.getElementById('formModal')) {
-        document.getElementById('formModal').style.display = 'none';
-    }
-}
-
 document.getElementById('waitlistForm').onsubmit = function(event) {
     event.preventDefault();
-    alert('Thank you for joining the waitlist!');
-    document.getElementById('formModal').style.display = 'none';
-}
+    fetch(this.action, {
+        method: this.method,
+        body: new FormData(this),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            alert('Thank you for joining the waitlist!');
+            document.getElementById('formModal').style.display = 'none';
+        } else {
+            alert('There was a problem submitting your form.');
+        }
+    });
+};
