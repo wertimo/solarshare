b3c144482d5408df7b7cf55d24447c54a1882fc2
wertimo <wertekq@gmail.com>
2024-06-03 00:41:09 +0100

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+document.getElementById('joinWaitlistButton').onclick = function() {
+    document.getElementById('formModal').style.display = 'flex';
+}
+
+document.getElementsByClassName('close')[0].onclick = function() {
+    document.getElementById('formModal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('formModal')) {
+        document.getElementById('formModal').style.display = 'none';
+    }
+}
+
 document.getElementById('waitlistForm').onsubmit = function(event) {
     event.preventDefault();
     fetch(this.action, {
