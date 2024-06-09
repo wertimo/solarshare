d20ebff38c23858d8f279deca33a54664fafc746
wertimo <wertekq@gmail.com>
2024-06-09 21:45:20 +0100

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 
 // Initialize Supabase
-const supabaseUrl = 'https://bxcliaujryahksdpbewc.supabase.co'';
+const supabaseUrl = 'https://bxcliaujryahksdpbewc.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4Y2xpYXVqcnlhaGtzZHBiZXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc5NTQ1NjQsImV4cCI6MjAzMzUzMDU2NH0.jMqHXkcGoDBvb0vW0aP9OyGcYndPHCu5EnNIwzFwKKQ';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
