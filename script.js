8e4baab2427e48917035ec26fa552445c3ff8e5c
wertimo <wertekq@gmail.com>
2024-06-09 21:30:10 +0100

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Initialize Supabase
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_KEY';
+const supabaseUrl = 'https://bxcliaujryahksdpbewc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4Y2xpYXVqcnlhaGtzZHBiZXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc5NTQ1NjQsImV4cCI6MjAzMzUzMDU2NH0.jMqHXkcGoDBvb0vW0aP9OyGcYndPHCu5EnNIwzFwKKQ';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 // Modal functionality
