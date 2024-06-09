f850611213da953842e929b7539303a95974632d
wertimo <wertekq@gmail.com>
2024-06-09 21:57:37 +0100

    script.js

--- script.js
+++ script.js
@@ -2,37 +2,6 @@
 // const supabaseUrl = 'https://bxcliaujryahksdpbewc.supabase.co';
 // const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4Y2xpYXVqcnlhaGtzZHBiZXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc5NTQ1NjQsImV4cCI6MjAzMzUzMDU2NH0.jMqHXkcGoDBvb0vW0aP9OyGcYndPHCu5EnNIwzFwKKQ';
 
-// Form submission
-document.getElementById('waitlistForm').onsubmit = async function(event) {
-    event.preventDefault();
-    console.log('Form submission started.');
-
-    const firstName = document.getElementById('firstName').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-
-    console.log('Form data:', { firstName, email, phone });
-
-    try {
-        const { data, error } = await supabase
-            .from('waitlist')
-            .insert([
-                { first_name: firstName, email: email, phone_number: phone }
-            ]);
-
-        if (error) {
-            throw error;
-        } else {
-            alert('Thank you for joining the waitlist!');
-            formModal.style.display = 'none';
-            console.log('Form submitted successfully.');
-        }
-    } catch (error) {
-        alert('There was a problem submitting your form.');
-        console.error('Error submitting form:', error);
-    }
-};
-
 // Supabase initialization
 const supabaseUrl = 'https://bxcliaujryahksdpbewc.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4Y2xpYXVqcnlhaGtzZHBiZXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc5NTQ1NjQsImV4cCI6MjAzMzUzMDU2NH0.jMqHXkcGoDBvb0vW0aP9OyGcYndPHCu5EnNIwzFwKKQ';
@@ -61,5 +30,34 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
+    // Form submission
+    document.getElementById('waitlistForm').onsubmit = async function(event) {
+        event.preventDefault();
+        console.log('Form submission started.');
 
+        const firstName = document.getElementById('firstName').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+
+        console.log('Form data:', { firstName, email, phone });
+
+        try {
+            const { data, error } = await supabase
+                .from('waitlist')
+                .insert([
+                    { first_name: firstName, email: email, phone_number: phone }
+                ]);
+
+            if (error) {
+                throw error;
+            } else {
+                alert('Thank you for joining the waitlist!');
+                formModal.style.display = 'none';
+                console.log('Form submitted successfully.');
+            }
+        } catch (error) {
+            alert('There was a problem submitting your form.');
+            console.error('Error submitting form:', error);
+        }
+    };
 });
