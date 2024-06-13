2f87c441e64cc0b9dd03fe0275442285aca28009
wertimo <wertekq@gmail.com>
2024-06-13 21:29:47 +0100

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-
-// const supabaseUrl = 'https://bxcliaujryahksdpbewc.supabase.co';
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4Y2xpYXVqcnlhaGtzZHBiZXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc5NTQ1NjQsImV4cCI6MjAzMzUzMDU2NH0.jMqHXkcGoDBvb0vW0aP9OyGcYndPHCu5EnNIwzFwKKQ';
-
-// Initialize Supabase
-const supabaseUrl = 'https://bxcliaujryahksdpbewc.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4Y2xpYXVqcnlhaGtzZHBiZXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc5NTQ1NjQsImV4cCI6MjAzMzUzMDU2NH0.jMqHXkcGoDBvb0vW0aP9OyGcYndPHCu5EnNIwzFwKKQ';
-const supabase = createClient(supabaseUrl, supabaseKey)
-
 // Modal functionality
 const joinWaitlistButton = document.getElementById('joinWaitlistButton');
 const formModal = document.getElementById('formModal');
@@ -28,34 +19,3 @@ window.onclick = function(event) {
         console.log('Clicked outside modal, closing modal.');
     }
 }
-
-// Form submission
-document.getElementById('waitlistForm').onsubmit = async function(event) {
-    event.preventDefault();
-    console.log('Form submission started.');
-
-    const firstName = document.getElementById('firstName').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-
-    console.log('Form data:', { firstName, email, phone });
-
-    try {
-        const { data, error } = await supabase
-            .from('waitlist')
-            .insert([
-                { first_name: firstName, email: email, phone_number: phone }
-            ]);
-
-        if (error) {
-            throw error;
-        } else {
-            alert('Thank you for joining the waitlist!');
-            formModal.style.display = 'none';
-            console.log('Form submitted successfully.');
-        }
-    } catch (error) {
-        alert('There was a problem submitting your form.');
-        console.error('Error submitting form:', error);
-    }
-};
